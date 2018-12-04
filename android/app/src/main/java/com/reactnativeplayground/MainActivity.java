ed1ef7044ad92d071743195ba646ca69e46d64ce
Jerome Neumanns <ulrellik@gmail.com>
2018-12-04 12:00:23 -0500

    Adding React Native Navigation

--- android/app/src/main/java/com/reactnativeplayground/MainActivity.java
+++ android/app/src/main/java/com/reactnativeplayground/MainActivity.java
@@ -1,15 +1,6 @@
 package com.reactnativeplayground;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "ReactNativePlayground";
-    }
+public class MainActivity extends NavigationActivity {
 }
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /** @format */
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 import App from './App';
 import { name as appName } from './app.json';
 import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => () => <Provider store={configureStore()}><App /></Provider>);
+
+Navigation.registerComponent(`${appName}.WelcomeScreen`, () => () => (
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>
+));
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: `${appName}.WelcomeScreen`,
+      },
+    },
+  });
+});
