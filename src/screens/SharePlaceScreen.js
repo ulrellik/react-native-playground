b46084c8410810fe45b228b3ae189d8da32814b9
Jerome Neumanns <ulrellik@gmail.com>
2018-12-31 18:14:22 -0500

    Add draft for ShareScreen

--- src/screens/SharePlaceScreen.js
+++ src/screens/SharePlaceScreen.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button } from 'react-native';
 
 import { addPlace } from '../store/actions';
 import Input from '../components/Input';
 
+const styles = StyleSheet.create({
+  placeholder: {
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 300,
+    width: '100%',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
 export class SharePlaceScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Input onPress={this.props.onAddPlace} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.placeholder}><Text>[Image]</Text></View>
+          <Button title="Pick Image" onPress={this.props.onAddPlace} />
+          <View style={styles.placeholder}><Text>[Map]</Text></View>
+          <Button title="Locate Me" onPress={this.props.onAddPlace} />
+          <Input onPress={this.props.onAddPlace} />
+        </View>
+      </ScrollView>
     );
   }
 }
